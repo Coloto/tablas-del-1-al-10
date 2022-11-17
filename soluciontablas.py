a3a05a18ec5f276ccc22c5b3a5ac2f6a64aa0279
Coloto <117723416+Coloto@users.noreply.github.com>
2022-11-17 09:36:17 +0100

    Update soluciontablas.py

--- soluciontablas.py
+++ soluciontablas.py
@@ -1,7 +1,6 @@
 print('Tablas de multiplicar')
-tablas=[1,2,3,4,5,6,7,8,9,10]
-for x in tablas:
+for x in range(1,10 +1)
     print(f'Tabla del {x}')
-    for y in range(10):
+    for y in range(1,10+1)
         resultado=x*y
         print(f'{x} * {y} es {resultado}')
